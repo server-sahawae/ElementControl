ed26551e3c112965b32fccdbb11db2d5f5370b9b
sahawaenya <saha.waenya@gmail.com>
2023-03-09 08:41:15 +0700

    First Commit

--- app.js
+++ app.js
@@ -5,22 +5,14 @@ const ErrorHandler = require("./middlewares/ErrorHandler");
 const routes = require("./routes");
 const app = express();
 const port = process.env.PORT || 3000;
-// const session = require("express-session");
 const cors = require("cors");
 app.use(cors({ origin: true, credentials: true }));
 app.set("trust proxy", 1);
 app.use(express.urlencoded({ extended: true, limit: 10485760 }));
 app.use(express.json());
-// app.use(
-//   session({
-//     secret: process.env.ENCRYPTION_KEY,
-//     resave: true,
-//     saveUninitialized: false,
-//   })
-// );
 
 app.use(routes);
 app.use(ErrorHandler);
 app.listen(port, () => {
-  console.log(`gloabalAccess listening on port ${port}`);
+  console.log(`Element Control listening on port ${port}`);
 });
