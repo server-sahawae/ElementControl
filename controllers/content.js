7775204cc17f36681d1d03590f89c5981f321474
sahawaenya <saha.waenya@gmail.com>
2023-03-10 17:04:59 +0700

    fix some missing variable

--- controllers/content.js
+++ controllers/content.js
@@ -3,7 +3,7 @@ const { APIGetNameByUserId } = require("../APIs/GlobalAccess");
 const redis = require("../config/redisConfig");
 const { NO_DATA, NO_UPDATE } = require("../constants/ErrorKeys");
 const { loggerInfo, loggerDebug } = require("../helpers/loggerDebug");
-const { redisDel, redisContent } = require("../helpers/redis");
+const Redis = require("../helpers/redis");
 const { Content } = require("../models");
 
 module.exports = class Controller {
@@ -11,7 +11,7 @@ module.exports = class Controller {
     try {
       const { ContentId } = req.params;
       const { CompanyId } = req.access;
-      const redisKey = redisContent(CompanyId, ContentId);
+      const redisKey = Redis.contentKey(CompanyId, ContentId);
       const { access_token } = req.headers;
       const itemRedis = JSON.parse(await redis.get(redisKey));
       let result = {};
@@ -54,7 +54,7 @@ module.exports = class Controller {
         { where: { id: ContentId } }
       );
       loggerDebug(`REDIS KEY: ${CompanyId}:${data.CategoryId}:${ContentId}`);
-      await redisDel(CompanyId, data.CategoryId, ContentId);
+      await Redis.del(CompanyId, data.CategoryId, ContentId);
       res.status(200).json({
         message: `${data.title} is now ${
           isActive != "false" ? "active" : "inactive"
