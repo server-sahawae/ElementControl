de627036faea263636e1b14eb608933c22a03506
sahawaenya <saha.waenya@gmail.com>
2023-03-10 19:35:36 +0700

    ip check

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const path = require("path");
+const requestIp = require("request-ip");
 
 const express = require("express");
 const routes = require("./routes");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-app.use(cors({ origin: true, credentials: true }));
-app.set("trust proxy", 1);
-app.use(express.urlencoded({ extended: true, limit: 10485760 }));
-app.use(express.json());
 const ErrorHandler = require("./middlewares/ErrorHandler");
 const { loggerInfo } = require("./helpers/loggerDebug");
 const redis = require("./config/redisConfig");
 
+app.use(cors({ origin: true, credentials: true }));
+app.set("trust proxy", 1);
+app.use(express.urlencoded({ extended: true, limit: 10485760 }));
+app.use(express.json());
+app.use(requestIp.mw());
+
 app.get("/log", (req, res) => {
-  const ipAddress = req.socket.remoteAddress;
+  const ipAddress = req.clientIp;
   loggerInfo(`CHECK LOG from ${ipAddress}`);
   res.sendFile(path.join(__dirname + "/logs.log"));
 });
